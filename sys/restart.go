471249ed5ca88b5b32270f970da0212c02946238
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2026-09-25 14:16:53 +0800

    refactor: 移除未使用的重启辅助代码

--- sys/restart.go
+++ sys/restart.go
@@ -2,45 +2,12 @@ package sys
 
 import (
 	"os"
-	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/mattn/go-isatty"
 
 	"github.com/WindowsSov8forUs/glyccat/log"
 )
-
-// RestartApplication 重启应用
-func RestartApplication() {
-	executableName, err := GetExecutableName()
-	if err != nil {
-		log.Fatalf("获取可执行文件名时出错: %v", err)
-		os.Exit(1)
-	}
-
-	restarter := NewRestarter()
-	if err := restarter.Restart(executableName); err != nil {
-		log.Fatalf("重启应用时出错: %v", err)
-		os.Exit(1)
-	}
-
-	if _, err := os.Create("restart.flag"); err != nil {
-		log.Fatalf("创建重启标记文件时出错: %v", err)
-		os.Exit(1)
-	}
-
-	os.Exit(0)
-}
-
-// GetExecutableName 获取可执行文件名
-func GetExecutableName() (string, error) {
-	executable, err := os.Executable()
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSuffix(executable, filepath.Ext(executable)), nil
-}
 
 // isRunningInTerminal 检查是否在终端中运行
 func isRunningInTerminal() bool {
