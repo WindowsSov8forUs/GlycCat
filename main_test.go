51c211132c37b8bd9cb06edb216c21cee757e887
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2026-09-25 14:07:01 +0800

    fix: 按新配置传递 WebSocket 手动分片

--- main_test.go
+++ main_test.go
@@ -26,3 +26,19 @@ func TestNewRuntimeAcceptsMessageStore(t *testing.T) {
 		t.Fatal("Satori server was not created")
 	}
 }
+
+func TestNewRuntimeAcceptsManualWebSocketShard(t *testing.T) {
+	conf := config.DefaultConfig()
+	conf.Account.AppID = 123
+	conf.Account.AppSecret = "test-secret"
+	conf.Account.WebHook.Enable = false
+	conf.Account.WebSocket.Enable = true
+	shardID := uint32(0)
+	conf.Account.WebSocket.ShardID = &shardID
+	conf.Account.WebSocket.ShardCount = 2
+	bundle, err := newRuntime(conf, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer bundle.satoriServer.Close()
+}
